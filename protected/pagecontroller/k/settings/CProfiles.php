d99fa4003ac8333f466ed998255c82bf07c98c76
MOCHAMMAD RIZKI ROMDONI <cv.yacanet@gmail.com>
2016-05-30 07:06:49 +0700

    Profiles User keuangan
    Bisa ganti password

--- protected/pagecontroller/k/settings/CProfiles.php
+++ protected/pagecontroller/k/settings/CProfiles.php
@@ -7,24 +7,19 @@ class CProfiles extends MainPageK {
 		if (!$this->IsPostBack&&!$this->IsCallBack) {	
             if (!isset($_SESSION['currentPageCache'])||$_SESSION['currentPageCache']['page_name']!='k.settings.Profiles') {
 				$_SESSION['currentPageCache']=array('page_name'=>'k.settings.Profiles','page_num'=>0);												
-			}            
-            $this->populateData ();
+			}
 		}
-        
-	}   
-    public function populateData () {
-//        $this->cmbTheme->DataSource=$this->setup->getListThemes();
-//        $this->cmbTheme->Text=$_SESSION['theme'];
-//        $this->cmbTheme->DataBind();
-    }
-    
+	}
     public function saveData ($sender,$param) {
         if ($this->IsValid) {
-            $theme=$this->cmbTheme->Text;
-            $_SESSION['theme']=$theme;
-            $userid=$this->Pengguna->getDataUser('userid');
-            $str = "UPDATE simak_user SET theme='$theme' WHERE userid=$userid";            
-            $this->DB->updateRecord($str);
+           $id=$this->Pengguna->getDataUser('userid');                   
+            if ($this->txtPassword->Text != '') {                
+                $data=$this->Pengguna->createHashPassword($this->txtPassword->Text);
+                $salt=$data['salt'];
+                $password=$data['password'];
+                $str = "UPDATE user SET userpassword='$password',salt='$salt' WHERE userid=$id";
+            }
+            $this->DB->updateRecord($str);                       
             $this->redirect('settings.Profiles',true);
         }
     }

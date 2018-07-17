c377f73f20b6016308bb85d314be5251a01cbe84
MOCHAMMAD RIZKI ROMDONI <cv.yacanet@gmail.com>
2018-07-17 08:13:16 +0700

    Update portalekampus.sql

--- portalekampus.sql
+++ portalekampus.sql
@@ -1438,25 +1438,6 @@ CREATE TABLE `setting` (
   `value` text COLLATE latin1_general_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
--- --------------------------------------------------------
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Jul 13, 2018 at 06:13 AM
--- Server version: 5.6.39-cll-lve
--- PHP Version: 5.6.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
---
--- Database: `stisipol_portal`
---
-
 --
 -- Dumping data for table `setting`
 --
@@ -1519,6 +1500,17 @@ CREATE TABLE `status_mhs` (
   `n_status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `status_mhs`
+--
+
+INSERT INTO `status_mhs` (`k_status`, `n_status`) VALUES
+('A', 'AKTIF'),
+('C', 'CUTI'),
+('D', 'DROP-OUT / PUTUS'),
+('K', 'KELUAR'),
+('L', 'LULUS'),
+('N', 'NON-AKTIF');
 -- --------------------------------------------------------
 
 --
